f83cfe35642c19b5de456d979489fd431ff097d2
FahadKhanEtna <32654500+FahadKhanEtna@users.noreply.github.com>
2017-12-07 09:45:10 +0100

    list

---
+++ readme-instruction.txt
@@ -0,0 +1,10 @@
+Présentation de l'app
+- Creation d'une liste d'utilisateur
+- Un détail des utilisateurs apparait au click
+- Utilisation de Redux
+
+Pour lancer l'application
+- ouvrir la console
+- aller dans le dossier de l'app
+- lancer la commande npm install
+- lancer la commande npm start
---
+++ src/reducers/reducer_users.js
@@ -0,0 +1,9 @@
+export default function(){
+
+             return [
+                        {name:'Fahad Khan',id:1,role:'ROOT',active:'NON'},
+                        {name:'Asad Khan',id:2,role:'USER',active:'OUI'},
+                        {name:'Youns Khan',id:4,role:'ADMIN',active:'OUI'},
+                    ] 
+    }
+   
